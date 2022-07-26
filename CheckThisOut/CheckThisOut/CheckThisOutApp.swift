ee77fa1062c9d86c7fa2165f8c18a0c396222fe3
verasazonova <vera.zhokhova@gmail.com>
2022-07-26 14:31:50 -0700

    Setup project

---
+++ CheckThisOut/CheckThisOut/CheckThisOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckThisOutApp.swift
+//  CheckThisOut
+//
+//  Created by Vera Sazonova on 7/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CheckThisOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
